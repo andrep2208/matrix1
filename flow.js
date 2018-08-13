863856d1e858b003cdd29e64a52fdf3cfe3a0566
Andre Pham <andrepham@Andres-MacBook-Pro.local>
2018-08-13 22:23:56 +1200

    update

--- flow.js
+++ flow.js
@@ -1,25 +1,27 @@
 function Flow() {
-    let randomplace = floor(random(0, 30))*20
+    let randomplace = floor(random(0, 19))*30
     this.numberofword  = floor(random(1, 15));
     let randomstart = floor(random(0,100))
     this.word = [];
     this.maxword = 15;
+    this.speed = floor(random(0,5));
 
     for (i= 0; i < this.maxword ; i++) {
-      this.word[i] = new Word(i*wordSize,randomplace, randomstart);
+      this.word[i] = new Word(i*wordSize,randomplace, randomstart,this.speed);
     }
 
 
     this.render = function () {
+        
         for (i = 0; i < this.numberofword; i++){
             this.word[i].render();
             this.word[i].update();
         }
-
+        
     }
 
     this.replay = function() {
-    if (this.word[this.numberofword-1].y -this.word[this.numberofword-1].a -this.word[this.numberofword-1].b == 600 + wordSize) {
+    if (this.word[this.numberofword-1].y -this.word[this.numberofword-1].a -this.word[this.numberofword-1].b > 600 + wordSize) {
         //console.log('true')
         this.numberofword = floor(random(1, 15));
         for (i = 0; i < this.word.length; i++){
--- word.js
+++ word.js
@@ -1,13 +1,13 @@
-function Word (a, x, b) {  
+function Word (a, x, b,speed) {  
   
     let randomnum;
     this.x = x;
     this.y = 0;
     this.gravity = 0;
-    this.velocity = 1;
+    this.velocity = 1 + speed;
     this.a = a;
     this.b = b;
-  
+    this.glow = round(random(0,1));
   
     this.render = function() {
       textSize(wordSize);
@@ -15,9 +15,21 @@ function Word (a, x, b) {
       if (frameCount % 20 == 0 ){
         randomnum = random()*33
       }
-  
+
       text(String.fromCharCode(3e4+randomnum), this.x, this.y -a - b);
+      push();
       fill(255,0,0);
+
+      if (this.glow == 0){
+        stroke('white');
+        pop();
+      }
+     
+    }
+
+    this.glow = function () {
+      
+      stroke('white');
     }
   
     this.update = function() {
