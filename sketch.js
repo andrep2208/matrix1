ae0e4853f0c67e3073a17899e23fe3eac7f567c2
Andre Pham <andrepham@Andres-MacBook-Pro.local>
2018-08-14 20:29:53 +1200

    update

--- sketch.js
+++ sketch.js
@@ -1,29 +1,23 @@
-
 var flow = [];
-// var flow1;
 var wordSize = 30;
+var numberofflow = 50;
+
+
 function setup () {
   createCanvas(800,600); 
-  for (let i = 0; i < 10; i++){
+  for (let i = 0; i < numberofflow; i++){
     flow[i] = new Flow();
   }
-  // flow1 = new Flow();
-
 }
 
-var randomnum;
+
 function draw() {
   background(0);
 
- for(let i = 0; i < flow.length; i++) {
+  for(let i = 0; i < flow.length; i++) {
    flow[i].render();
    flow[i].replay();
  }
-    //flow.render();
-    // flow1.render();
-    // flow1.replay();
-
-
 
 
 }
--- word.js
+++ word.js
@@ -1,40 +1,39 @@
 function Word (a, x, b,speed) {  
   
-    let randomnum;
-    this.x = x;
-    this.y = 0;
-    this.gravity = 0;
-    this.velocity = 1 + speed;
-    this.a = a;
-    this.b = b;
-    this.glow = round(random(0,1));
-  
-    this.render = function() {
-      textSize(wordSize);
-  
-      if (frameCount % 20 == 0 ){
-        randomnum = random()*33
-      }
+  let randomnum;
+  this.x = x;
+  this.y = 0;
+  this.gravity = 0;
+  this.velocity = 1 + speed;
+  this.a = a;
+  this.b = b;
+  this.glow = round(random(0,0.51));
 
-      text(String.fromCharCode(3e4+randomnum), this.x, this.y -a - b);
-      push();
-      fill(255,0,0);
+  this.render = function(fade) {
+    textSize(wordSize);
 
-      if (this.glow == 0){
-        stroke('white');
-        pop();
-      }
-     
+    if (frameCount % 15 == 0 ){
+      randomnum = random()*33
     }
 
-    this.glow = function () {
-      
+    push()
+
+    fill(255-fade,0,0);
+
+    if (this.glow == 1) {
       stroke('white');
+      strokeWeight(3.5 - fade/200);
+      smooth();
     }
-  
-    this.update = function() {
-      //this.gravity += this.velocity;
-      this.y += this.velocity;
-    }
-  
+    textStyle(BOLD)
+    text(String.fromCharCode(0x30A0+randomnum), this.x, this.y -a - b);
+    pop();
+    
   }
+
+  this.update = function() {
+    this.gravity += this.velocity;
+    this.y += this.velocity;
+  }
+
+}
